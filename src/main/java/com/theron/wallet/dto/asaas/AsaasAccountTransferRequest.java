35c2163a722fe1607900ae250736555795c68f58
ACN-1277 <datacode.cn@gmail.com>
2026-08-27 13:33:37 -0300

    feat(payment-order): add Asaas account transfer request

---
+++ src/main/java/com/theron/wallet/dto/asaas/AsaasAccountTransferRequest.java
@@ -0,0 +1,22 @@
+package com.theron.wallet.dto.asaas;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AsaasAccountTransferRequest {
+
+    private BigDecimal value;
+    private String walletId;
+}
